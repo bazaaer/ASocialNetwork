635656889c05bffa6ceda6d921633211cf77ad84
Lander Van der Stighelen <lander@vanderstighelen.net>
2024-10-22 11:32:31 +0000

    Add create_post endpoint to API

--- api/model.py
+++ api/model.py
@@ -6,3 +6,7 @@ api_blueprint = Blueprint('api', __name__)
 @api_blueprint.route('/api', methods=['GET'])
 def index():
     return 'ASocialNetwork API is running!'
+
+@api_blueprint.route('/api/post', methods=['POST'])
+def create_post():
+    return 'Create post'
